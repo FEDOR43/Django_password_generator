adc367131a4f9b8ab373ed350d541796cf98d56e
Fedorov Anton <afedorov43@gmail.com>
2022-04-14 19:21:10 +0300

    Added new url.

--- password_generator/generator/views.py
+++ password_generator/generator/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(requests):
+    return render(requests, 'generator/index.html')
+
+
+def password(requests):
+    return render(requests, 'generator/password.html')
--- password_generator/password_generator/urls.py
+++ password_generator/password_generator/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from generator import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('password/', views.password, name='password'),
+    path('', views.index),
+
 ]
