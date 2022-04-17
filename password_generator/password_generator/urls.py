1beeaaaa5a38b5ba78fceae60a1cf1f2305b4cd4
Fedorov Anton <afedorov43@gmail.com>
2022-04-17 14:56:27 +0300

    Added auto-selection of password settings, fixed import in urls.py

--- password_generator/password_generator/urls.py
+++ password_generator/password_generator/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from generator import views
+from password_generator.generator import views
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
